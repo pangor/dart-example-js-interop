43bbae00ba2156d00cd5849daf09b1525f39e671
Seth Ladd <sethladd@google.com>
2013-06-11 22:01:38 +0200

    enable --minify, remove unnecessary scope

--- web/dart_js_interop_example.dart
+++ web/dart_js_interop_example.dart
@@ -3,13 +3,14 @@ import 'package:js/js.dart' as js;
 
 void main() {
   var context = js.context;
-  js.scoped(() {
-    var hug = new js.Proxy(context.Hug);
-    var result = hug.embrace(10);
-    query('#output').text = result;
-    
-    hug.patBack(new js.Callback.once((msg) {
-      query('#output').appendText(' This just in: $msg');
-    }));
-  });
+  
+  // use the [] notation to let --minify work
+  // see https://github.com/dart-lang/js-interop/issues/86
+  var hug = new js.Proxy(context['Hug']);
+  var result = hug['embrace'](10);
+  query('#output').text = result;
+  
+//    hug.patBack(new js.Callback.once((msg) {
+//      query('#output').appendText(' This just in: $msg');
+//    }));
 }
